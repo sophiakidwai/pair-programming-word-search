deeade2102a3ee1cf1e2399724e924cc9c92582f
sophia kidawai <sophia.kidwai@telus.com>
2022-11-23 18:59:05 -0800

    pair programming word search

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,13 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
-    }
+const wordSearch = (letters, word) => {
+  let x = 0;
+  const verticalJoin = letters.map(item => item[x]).join('');
+  const horizontalJoin = letters.map(ls => ls.join(''))
+  for (l of horizontalJoin) {
+      if (l.includes(word)) return true
+  }
+  for (item of verticalJoin) {
+      x++;
+      if (!item.includes(word)) return false
 }
-
-module.exports = wordSearch
+}
+module.exports = wordSearch;
